46d02e99a008f8c20648d31d9eca3a4fa07ebea3
Chloe A <chloe@sapphic.moe>
2026-01-02 11:21:54 +0500

    chore: add ModalStateProvider to root

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
 import { ConfirmDialogProvider } from '@/providers/ConfirmDialogProvider';
+import { ModalStateProvider } from '@/providers/ModalStateProvider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ConfirmDialogProvider>
-      <App />
-    </ConfirmDialogProvider>
+    <ModalStateProvider>
+      <ConfirmDialogProvider>
+        <App />
+      </ConfirmDialogProvider>
+    </ModalStateProvider>
   </React.StrictMode>
 );
