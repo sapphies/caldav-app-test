ddf7081bb1bf1cfa1ae4c4bde5d38bd9d2bd32d9
Chloe A <chloe@sapphic.moe>
2026-01-03 08:18:08 +0500

    feat: try to not show white flash on app load

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,13 @@ import './styles/index.css';
 import { ConfirmDialogProvider } from '@/providers/ConfirmDialogProvider';
 import { ModalStateProvider } from '@/providers/ModalStateProvider';
 
+// delay showing the window to prevent white flash on startup
+// should experiment with the exact timing here
+// 100ms seems to work ok on my macbook but i'll try it on windows and fedora later...
+setTimeout(async () => {
+  (await import('@tauri-apps/api/window')).getCurrentWindow().show();
+}, 100);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ModalStateProvider>
